8be29bd36dfae4ddebd9e9e6936d62f0370fad58
Marcus Dicander <dicander@kth.se>
2016-09-13 09:49:20 +0200

    added a trashed example.

--- trash_alexanders_example.cpp
+++ trash_alexanders_example.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 struct base {
-  virtual base foo() {
-    cout << "base::foo() "; return *this;
+  virtual base* foo() {
+    cout << "base::foo() "; return this;
   }
 };
 struct derived : base {
-  virtual base foo() {
-    cout << "derived::foo() "; return *this;
+  virtual base* foo() {
+    cout << "derived::foo() "; return this;
   }
 
-  derived goo() {
-    cout << "derived::goo()"; return *this;
+  derived* goo() {
+    cout << "derived::goo()"; return this;
   }
 };
 int main() {
@@ -21,6 +21,6 @@ int main() {
   cout << endl << "a) "; derived_object.foo();
   cout << endl << "b) "; base_object.foo();
   cout << endl << "c) "; ref.foo();
-  cout << endl << "d) "; ref.foo().foo();
-  cout << endl << "e) "; derived_object.goo().foo();cout << endl;
+  cout << endl << "d) "; ref.foo()->foo();
+  cout << endl << "e) "; derived_object.goo()->foo();cout << endl;
 }
