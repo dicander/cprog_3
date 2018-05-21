19a4b695c45943c4541f75324e878bf164fd6ceb
Marcus Dicander <dicander@kth.se>
2018-05-21 13:55:31 +0200

    Added really really evil examples of virtual

---
+++ evil_virtual.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+struct base {
+  virtual base& foo() {
+    cout << "base::foo() "; return *this;
+  }
+};
+struct derived : base {
+  virtual base& foo() {
+    cout << "derived::foo() "; return *this;
+  }
+
+  derived goo() {
+    cout << "derived::goo()"; return *this;
+  }
+};
+int main() {
+  derived derived_object;
+  base base_object = derived_object;
+  base & ref = derived_object;
+  cout << endl << "a) "; derived_object.foo();
+  cout << endl << "b) "; base_object.foo();
+  cout << endl << "c) "; ref.foo();
+  cout << endl << "d) "; ref.foo().foo();
+  cout << endl << "e) "; derived_object.goo().foo();cout << endl;
+}
