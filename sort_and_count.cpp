1c4d284949c6aa3e5fcab5733245f57ccdc298d2
Marcus Dicander <dicander@kth.se>
2018-05-21 15:38:39 +0200

    Fixed minor indentation issues

--- sort_and_count.cpp
+++ sort_and_count.cpp
@@ -5,24 +5,24 @@
 using namespace std;
 
 int main() {
-	cin.sync_with_stdio(true);
-	vector<int> v;
-	int just_read;
-	while(cin>>just_read) {
-		v.push_back(just_read);
-	}
-	sort(v.begin(), v.end());
-	int count = v[0];
-	bool all_there = true;
-	for(auto it=v.begin(); it!=v.end(); ++it) {
-		if(count!=*it) {
-			cout << "expected " << count << " found " << *it << endl;
-			all_there = false;
-		}
-		count++;
-	}
-	if(all_there) {
-		cout << "Found " << v.size() << " ordered numbers" << endl;
-	}
+    cin.sync_with_stdio(true);
+    vector<int> v;
+    int just_read;
+    while(cin>>just_read) {
+        v.push_back(just_read);
+    }
+    sort(v.begin(), v.end());
+    int count = v[0];
+    bool all_there = true;
+    for(auto it=v.begin(); it!=v.end(); ++it) {
+        if(count!=*it) {
+            cout << "expected " << count << " found " << *it << endl;
+            all_there = false;
+        }
+        count++;
+    }
+    if(all_there) {
+        cout << "Found " << v.size() << " ordered numbers" << endl;
+    }
 
 }
